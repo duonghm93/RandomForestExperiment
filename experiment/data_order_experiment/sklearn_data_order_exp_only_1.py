b82b1cd010c25f71f037c72fb6d750295a32f764
Duong Hoang-Minh <duonghm@runsystem.net>
2017-12-01 14:46:03 +0700

    [data_order_change]experiment result

--- experiment/data_order_experiment/sklearn_data_order_exp_only_1.py
+++ experiment/data_order_experiment/sklearn_data_order_exp_only_1.py
@@ -32,12 +32,23 @@ def get_df_from_list_csv_files(filenames):
 
 
 if __name__ == '__main__':
-    log_file = constant.get_data_order_change_experiment_log_file()
-    # with open(log_file, 'w+') as fp:
-    #     fp.write('strategy_name, auroc\n')
+    train_size = 100
+    test_size = 10
+    num_tree = 40
+    max_depth=10
+    sub_feature = 'sqrt'
+    random_state=0
+    bootstrap=False
+    oob_score=False
 
-    df_train = get_df_from_list_csv_files(constant.get_train_files(2,10,6))
-    df_test = get_df_from_list_csv_files(constant.get_test_files(2,10,6))
+    str_detail = '{}_{}_{}_{}_{}_{}_{}_{}'.format(train_size, test_size, num_tree, max_depth, sub_feature, random_state, bootstrap, oob_score)
+
+    log_file = constant.get_data_order_change_experiment_log_file(addition_infor=str_detail)
+    with open(log_file, 'w+') as fp:
+        fp.write('strategy_name, auroc\n')
+
+    df_train = get_df_from_list_csv_files(constant.get_train_files(2,train_size,6))
+    df_test = get_df_from_list_csv_files(constant.get_test_files(2,test_size,6))
     print('Train size: ', df_train.shape[0])
     print('Test size: ', df_test.shape[0])
 
@@ -49,21 +60,18 @@ if __name__ == '__main__':
     features_column_names = constant.FEATURES_COLUMNS_NAMES
     label_column_name = 'is_click'
 
-    # for strategy_name, strategy_function in data_order_strategy.strategies.items():
-    data_order_strategies = data_order_strategy._sort_strategy_generator(df_train)
-    for strategy in data_order_strategies:
+    for col_name in (['nothing'] + list(df_train.columns)):
         gc.collect()
-        strategy_name = strategy[0]
-        strategy_function = strategy[1]
-        print('=== {}'.format(strategy_name))
-
-        df_train_sorted = strategy_function(df_train)
-        # df_train_sorted = drop_ng_row(df_train_sorted)
+        print('=== {}'.format(col_name))
+        if col_name == 'nothing':
+            df_train_sorted = df_train
+        else:
+            df_train_sorted = df_train.sort_values(col_name)
 
         rf = RandomForestClassifier(
-            n_estimators=40, max_depth=10,
-            max_features='sqrt', random_state=0,
-            bootstrap=True, oob_score=False, n_jobs=-1
+            n_estimators=num_tree, max_depth=max_depth,
+            max_features=sub_feature, random_state=random_state,
+            bootstrap=bootstrap, oob_score=oob_score, n_jobs=-1
         )
 
         train_features = df_train_sorted[features_column_names]
@@ -74,8 +82,8 @@ if __name__ == '__main__':
         predicts = [x[1] for x in rf.predict_proba(df_test[features_column_names])]
 
         auroc = metrics.roc_auc_score(y_true=df_test[label_column_name], y_score=predicts)
-        # with open(log_file, 'a') as fp:
-        #     fp.write('{},{}\n'.format(strategy_name,auroc))
+        with open(log_file, 'a') as fp:
+            fp.write('{},{}\n'.format(col_name,auroc))
 
-        print(strategy_name, ':', auroc)
+        print(col_name, ':', auroc)
 
